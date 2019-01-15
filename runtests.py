2bd766a53ef2632563e1d7995a1d544677ecee68
Bob Donahue <bob_donahue@wgbh.org>
2019-01-15 13:56:13 -0500

    renaming test_settings to settings

--- runtests.py
+++ runtests.py
@@ -5,7 +5,7 @@ import django
 from django.conf import settings
 from django.test.utils import get_runner
 if __name__ == "__main__":
-    os.environ['DJANGO_SETTINGS_MODULE'] = 'gatekeeper.tests.test_settings'
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'gatekeeper.tests.settings'
     django.setup()
     TestRunner = get_runner(settings)
     test_runner = TestRunner()
